9b60241c77baa6900363243e563f059d49ab8bcf
lutfisobri <lutfilaki@gmail.com>
2022-04-13 12:40:52 +0700

    Upload

---
+++ src/utility/Date.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utility;
+
+/**
+ *
+ * @author LenataHoma
+ */
+public class Date {
+//    implements java.io.Serializable, Cloneable, Comparable<Date>{
+//    
+//    public int compareTo(Date anotherDate){
+//        long thisTime = getMillisOf(this);
+//      long anotherTime = getMillisOf(anotherDate);
+//      return (thisTime<anotherTime ? -1 : (thisTime==anotherTime ? 0 : 1));
+//    }
+
+    
+
+}
+
