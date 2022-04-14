7d9fa41392b31da9af3ad5f2ff66adaf276566f7
lutfisobri <lutfilaki@gmail.com>
2022-04-14 08:42:31 +0700

    desktop pane

---
+++ src/utility/save_akun.java
@@ -0,0 +1,23 @@
+
+package utility;
+
+public class save_akun {
+    public String user;
+    public String pass;
+    
+    public void setUser(String username){
+        user = username;
+    }
+    
+    public String getUser(){
+        return user;
+    }
+    
+    public void setPass(String password){
+        pass = password;
+    }
+    
+    public String getPass(){
+        return pass;
+    }
+}
