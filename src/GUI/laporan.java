2e54621aba0fee2a17dc446e635863c7cc60fdc8
Fathan Maulana <fthnmrh012@gmail.com>
2022-04-21 10:03:45 +0700

    ini Fathan lagi2

--- src/GUI/laporan.java
+++ src/GUI/laporan.java
@@ -5,8 +5,15 @@
  */
 package GUI;
 
+import db.konekdb;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
 import javax.swing.plaf.basic.BasicInternalFrameUI;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -23,7 +30,46 @@ public class laporan extends javax.swing.JInternalFrame {
         BasicInternalFrameUI gui = (BasicInternalFrameUI) this.getUI();
         gui.setNorthPane(null);
         jScrollPane1.getVerticalScrollBar().setUnitIncrement(16);
+        
+        
+        
+        jTable1.getTableHeader().setFont(new Font("Quicksand Light", Font.BOLD, 12));
+        jTable1.getTableHeader().setOpaque(false);
+        jTable1.getTableHeader().setBackground(new Color(254,149,46));
+        jTable1.getTableHeader().setForeground(new Color(255,255,255));
+        loadTable();
+        
     }
+    
+    private void loadTable(){
+        Border empty = BorderFactory.createEmptyBorder();
+        DefaultTableModel table2 = new DefaultTableModel();
+        table2.addColumn("Tanggal Transaksi");
+        table2.addColumn("Jumlah Laba");
+        try {
+            String sql = "SELECT tb_transjual.tgl_transaksi,tb_detail_transjual.id_barang, SUM((tb_barang.harga_jual - tb_barang.harga_dasar) * tb_detail_transjual.jumlah) AS laba\n"
+                    + "FROM tb_detail_transjual\n"
+                    + "JOIN tb_transjual\n"
+                    + "ON tb_transjual.id_transjual = tb_detail_transjual.id_transjual\n"
+                    + "JOIN tb_barang\n"
+                    + "ON tb_barang.id_barang = tb_detail_transjual.id_barang\n"
+                    + "GROUP BY tb_transjual.tgl_transaksi;";
+            java.sql.Connection conn = (Connection)konekdb.GetConnection();
+            java.sql.Statement stm = conn.createStatement();
+            java.sql.ResultSet rs = stm.executeQuery(sql);
+            while (rs.next()) {                
+                table2.addRow(new Object[]{
+                    rs.getString("tgl_transaksi"),
+                    rs.getString("laba")
+                });
+            jTable1.setModel(table2);
+            jTable1.setBorder(empty);
+            }
+        } catch (Exception e) {
+            
+        }
+    }
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -36,21 +82,97 @@ public class laporan extends javax.swing.JInternalFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jPanel2 = new javax.swing.JPanel();
+        jPanel5 = new javax.swing.JPanel();
+        jPanel4 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
         jLabel3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
 
+        setBorder(null);
+        setPreferredSize(new java.awt.Dimension(962, 710));
         getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.LINE_AXIS));
 
+        jScrollPane1.setBorder(null);
         jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jPanel5.setBackground(new java.awt.Color(248, 248, 248));
+
+        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
+        jPanel5.setLayout(jPanel5Layout);
+        jPanel5Layout.setHorizontalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 850, Short.MAX_VALUE)
+        );
+        jPanel5Layout.setVerticalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 220, Short.MAX_VALUE)
+        );
+
+        jPanel2.add(jPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 760, -1, 220));
+
+        jPanel4.setBackground(new java.awt.Color(248, 248, 248));
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 850, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 220, Short.MAX_VALUE)
+        );
+
+        jPanel2.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 160, -1, 220));
+
+        jPanel3.setBackground(new java.awt.Color(248, 248, 248));
+
+        jTable1.setFont(new java.awt.Font("Quicksand Medium", 0, 11)); // NOI18N
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jTable1.setEnabled(false);
+        jTable1.setRowHeight(35);
+        jTable1.setRowSelectionAllowed(false);
+        jTable1.setSelectionBackground(new java.awt.Color(245, 121, 0));
+        jTable1.setShowVerticalLines(false);
+        jTable1.getTableHeader().setResizingAllowed(false);
+        jTable1.getTableHeader().setReorderingAllowed(false);
+        jScrollPane2.setViewportView(jTable1);
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 792, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(29, Short.MAX_VALUE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(43, Short.MAX_VALUE))
+        );
+
+        jPanel2.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 400, 850, 340));
+
         jLabel3.setBackground(new java.awt.Color(238, 238, 238));
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Background/bg_laporan.jpg"))); // NOI18N
-        jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 30, 950, 690));
-
-        jButton1.setText("jButton1");
-        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 800, -1, -1));
+        jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 950, 690));
 
         jScrollPane1.setViewportView(jPanel2);
 
@@ -61,9 +183,13 @@ public class laporan extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
